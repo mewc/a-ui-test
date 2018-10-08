daa378f6f26ac32cfecb1b4e0bd254074fb8242c
mewc <m@mewc.info>
2018-10-08 23:49:17 +1100

    style cards partly

--- src/js/FeedCard.js
+++ src/js/FeedCard.js
@@ -4,19 +4,33 @@ import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Avatar from "@material-ui/core/Avatar/Avatar";
+import ta from 'time-ago';
+import person1 from '../assets/1.jpg'
 
 const styles = theme => ({
     root: {
         ...theme.mixins.gutters(),
-        paddingTop: '15px',
-        paddingBottom: '15px',
-        margin: '15px',
+        margin: '15px 15px 40px 15px',
         width: '70%',
         float: 'right',
+        border: '5px solid transparent',
+        borderImage: 'linear-gradient(to bottom, #3acfd5 22%, #3a4ed5 100%)',
+        borderImageSlice: 1,
+
+},
+    avatar: {
+        margin: '10px',
     },
 });
 
 class FeedCard extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            winner: props.status
+        }
+    }
 
     render() {
         const {classes} = this.props;
@@ -24,12 +38,15 @@ class FeedCard extends React.Component {
 
         return <div>
             <Paper className={classes.root} elevation={1}>
-                <Avatar></Avatar>
-                <Typography variant="h3" component="h3">
+                <Avatar alt={'person'}><person1/></Avatar>
+                <Typography variant="h5" component="h5">
                     {this.props.item.for}
                 </Typography>
-                <Typography variant="h5" component="h3">
+                <Typography variant="p" component="subtitle">
                     {this.props.item.participants[0].name + ' vs ' + this.props.item.participants[1].name}
+                </Typography>
+                <Typography variant="subheading">
+                    {ta.ago(new Date()-this.props.item.timestamp)}
                 </Typography>
             </Paper>
         </div>
