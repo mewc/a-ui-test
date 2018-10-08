1fedb6410169db3349c2bb7360db5ebb3f0fb780
mewc <m@mewc.info>
2018-10-08 21:24:14 +1100

    add top nav

--- src/js/App.js
+++ src/js/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
 import '../css/App.css';
-import {BottomNavigation, BottomNavigationAction, AppBar, Typography, IconButton, Toolbar, Button} from '@material-ui/core'
+import {BottomNavigation, BottomNavigationAction, AppBar, Typography, IconButton, Toolbar} from '@material-ui/core'
+import {AssignmentOutlined, Add, LibraryAddOutlined, PersonOutlined, ChatBubbleOutline, ArrowBackIos} from '@material-ui/icons'
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tab: 0,
+            title: "feed"
+        }
+    }
   render() {
+      const styles = {
+          root: {
+              flexGrow: 1,
+          },
+          grow: {
+              flexGrow: 1,
+          },
+          menuButton: {
+              marginLeft: -12,
+              marginRight: 20,
+          },
+      };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <AppBar position="static">
+              <Toolbar>
+                  <IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+                      <ArrowBackIos />
+                  </IconButton>
+                  <Typography variant="h6" color="inherit" className={styles.grow}>
+                      {this.state.title}
+                  </Typography>
+              </Toolbar>
+          </AppBar>
+
           <BottomNavigation className={"BottomNav"}>
               <BottomNavigationAction label="Feed" value="0" icon={<AssignmentOutlined/>} />
               <BottomNavigationAction label="Log" value="1" icon={<LibraryAddOutlined/>} />
