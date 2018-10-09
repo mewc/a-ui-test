436cd82fee6bc4baa41f64ca5be2739a4cd88c27
mewc <m@mewc.info>
2018-10-09 21:22:51 +1100

    initial wager paper

--- src/js/Wager.js
+++ src/js/Wager.js
@@ -5,19 +5,16 @@ import FeedCard from "./FeedCard";
 import Paper from "@material-ui/core/Paper/Paper";
 import Grid from "@material-ui/core/Grid/Grid";
 import {withStyles} from "@material-ui/core";
+import AvatarItem from "./AvatarItem";
 
 
 let styles = theme => ({
     root: {
         ...theme.mixins.gutters(),
-        margin: '15px 15px 40px 15px',
-        width: '70%',
-        float: 'right',
-        border: '2px solid transparent',
-        borderImage: 'linear-gradient(to right, rgba(177,222,247,1) 0%, rgba(32,124,229,1) 100%)',
-        borderImageSlice: 1,
+        width: '100%',
+        height: '30%',
         flexGrow: 1,
-        padding: '10px'
+        padding: '30px'
     },
     avatar: {
         margin: '10px',
@@ -26,10 +23,19 @@ let styles = theme => ({
 
 class Wager extends Component {
     render() {
+        let {classes} = this.props;
         return <React.Fragment>
-            <Paper>
-                <Grid>
-
+            <Paper className={classes.root}>
+                <Grid container spacing={12} justify={'center'}>
+                    <Grid item xs>
+                        <AvatarItem src={'/1.jpg'} />
+                    </Grid>
+                    <Grid item xs>
+                        vs
+                    </Grid>
+                    <Grid item xs>
+                        <AvatarItem src={'/2.jpg'}/>
+                    </Grid>
                 </Grid>
             </Paper>
         </React.Fragment>
